d6732099670abc162e7e208e023f4bd53e2bea99
Jaisree07 <jaisree.anandhakumar@psiog.com>
2025-08-15 23:45:17 +0530

    add

--- trafffic.cpp
+++ trafffic.cpp
@@ -1,31 +1,68 @@
 #include<iostream>
 using namespace std;
 
+void color(){
+    string choose;
+    cout << "Choose the color" << endl;
+    cin >> choose;
+    if(choose=="red"){
+        return;
+    }
+     color(); 
+}
+void traffic(){
+         char iftraffic;
+         cout << "Traffic light present?yes(y) or no(n)" << endl;
+         cin >> iftraffic;
+         if(iftraffic){
+            color();
+         }
+         else{
+            cout << "Wait" << endl;
+         }
+
+}
 
 int main(){
     char isfound;
-    cout << "Found Footbridge?yes(y) or no(n)" << endl;
+    while(true){
+            cout << "Found Footbridge?yes(y) or no(n)" << endl;
     cin >> isfound;
     if(isfound=='y' || isfound=='Y'){
         cout << "Use footbridge" << endl;
+        break;
     }
     else{
         char istunnel;
         cout << "Found Tunnel?yes(y) or no(n)" << endl;
         cin >> istunnel;
-        if(istunnel=='y' || istunnel='Y'){
+        if(istunnel=='y' || istunnel=='Y'){
             cout << "Use tunnel" << endl;
+            break;
         }
         else{
             char iscrossing;
              cout << "Found Crossing?yes(y) or no(n)" << endl;
              cin >> iscrossing;
-             if(iscrossing){
-                Traffic();
+             if(iscrossing=='y'){
+                break;
              }
              else{
                 cout << "Walk away" << endl;
              }
+            }
         }
     }
+    traffic();
+    while(true){
+        cout << "Look left" << endl;
+        bool iscarapproach;
+        cout << "Is car approaching?(0/1)" << endl;
+        cin >> iscarapproach;
+        if(iscarapproach==false){
+            cout << "Cross the traffic" << endl;
+            break;
+        }
+    }
+
 }
