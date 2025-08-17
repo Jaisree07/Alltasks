c9644e667259cc3cf882d83eb41d7e97d78110f9
Jaisree07 <jaisree.anandhakumar@psiog.com>
2025-08-17 18:08:10 +0530

    commit

---
+++ airplane.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+void securityscrening(){
+    cout << "Security screening process" << endl;
+    char check;
+    //m-metal and r-restricted access
+    cout << "Checking(m/r)" << endl;
+    if(check=='m'|| check=='M'){
+        cout << "It is a metal" << endl;
+        cout << "Hand search" << endl;
+        //dangerous goods
+    }
+    
+}
+
+
+
+
+int main(){
+    cout << "Checkin" << endl;
+    cout << "Drop the baggage" << endl;
+    securityscreening();
+}
